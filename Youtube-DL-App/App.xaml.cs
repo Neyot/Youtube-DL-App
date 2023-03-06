3d020bdd41f39d6caa414c3c3940845be1a96995
Neyot <Neyot@users.noreply.github.com>
2023-03-06 21:04:12 +0000

    Renamed solution to Youtube-DL-App

--- Youtube-DL-App/App.xaml.cs
+++ Youtube-DL-App/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Youtube_DL_Wrapper
+namespace Youtube_DL_App
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Youtube-DL-App/MainWindow.xaml.cs
+++ Youtube-DL-App/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Youtube_DL_Wrapper
+namespace Youtube_DL_App
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
