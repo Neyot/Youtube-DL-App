3856b313d3fc264f0f8b9f18ebde505e7c880509
Neyot <Neyot@users.noreply.github.com>
2023-03-10 16:44:30 +0000

    Began designing main window

--- Youtube-DL-App/ViewModel/MainViewModel.cs
+++ Youtube-DL-App/ViewModel/MainViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Youtube_DL_App.Mvvm;
 
 namespace Youtube_DL_App.ViewModel
 {
@@ -11,5 +12,52 @@ namespace Youtube_DL_App.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
+        private OptionsViewModel? options = new OptionsViewModel();
+
+        private string? url = string.Empty;
+
+        public MainViewModel()
+        {
+            this.DownloadCommand = new RelayCommand(this.DownloadUrl, this.CanDownloadUrl);
+        }
+
+        public RelayCommand DownloadCommand { get; set; }
+
+        public OptionsViewModel? Options
+        {
+            get => this.options;
+            set
+            {
+                this.Set(ref this.options, value);
+            }
+        }
+
+        public string? Url
+        {
+            get => this.url;
+            set
+            {
+                this.Set(ref this.url, value);
+            }
+        }
+
+        /// <summary>
+        /// Can <see cref="Url"/> be downloaded with <see cref="Options"/> ?
+        /// </summary>
+        /// <param name="notUsed">This parameter is not used.</param>
+        /// <returns><see langword="true"/> if allowed; otherwise, <see langword="false"/>.</returns>
+        private bool CanDownloadUrl(object? notUsed)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Download <see cref="Url"/> with <see cref="Options"/> .
+        /// </summary>
+        /// <param name="notUsed">This parameter is not used.</param>
+        private void DownloadUrl(object? notUsed)
+        {
+
+        }
     }
 }
---
+++ Youtube-DL-App/ViewModel/OptionsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Youtube_DL_App.ViewModel
+{
+    public class OptionsViewModel : ViewModelBase
+    {
+		private readonly List<string> audioFormatOptions = new()
+		{
+			"mp3",
+			"wav"
+		};
+
+		private bool extractAudio;
+
+		public List<string> AudioFormatOptions => this.audioFormatOptions;
+
+		public bool ExtractAudio
+		{
+			get => this.extractAudio;
+			set => this.Set(ref this.extractAudio, value);
+		}
+	}
+}
